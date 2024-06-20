e370f5bfbb2d3f9c78b4b2f204aecb456ed56984
Rifi <121685704+pardalr@users.noreply.github.com>
2024-06-20 11:17:08 +0200

    Delete 23.3_Podcast directory

--- 23.3_Podcast/Query.sql
+++
@@ -1,2 +0,0 @@
-SELECT usuario.nombre, descargas.fecha, podcast.nombre FROM descargas, podcast, usuario
-WHERE usuario.idUsuario = descargas.Usuario_idUsuario AND descargas.Podcast_idPodcast = podcast.idPodcast;
